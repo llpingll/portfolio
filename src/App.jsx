cb8c1ef08b3b1ceb9520f9edc503c0ed13bd6221
llpingll <94099281+llpingll@users.noreply.github.com>
2024-12-02 01:17:24 +0000

    initial commit after installing threeJS, drei & styled components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+import styled from "styled-components";
+
+const App = () => {
+  return <Canv>App</Canv>;
+};
+
+const Canv = styled(Canvas)`
+  height: "100vh";
+  width: "100vw";
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default App;
